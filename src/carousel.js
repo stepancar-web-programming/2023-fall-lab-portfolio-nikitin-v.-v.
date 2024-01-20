ec918dcf9dc744edee124464715d6ed1b2896043
owlahikit <owlahikit@gmail.com>
2024-01-21 01:55:31 +0300

    linters,formatters and github actions add, css fixed

--- src/carousel.js
+++ src/carousel.js
@@ -1,19 +1,19 @@
 const arrayCarousel = [
-  "img/la/la1.jpg",
-  "img/la/la2.jpg",
-  "img/la/la3.jpg",
-  "img/la/la5.jpg",
-  "img/la/la6.jpg",
-  "img/la/la7.jpg",
-  "img/la/la8.jpg",
-  "img/la/la8.jpg",
-  "img/la/la9.jpg",
+  'img/la/la1.jpg',
+  'img/la/la2.jpg',
+  'img/la/la3.jpg',
+  'img/la/la5.jpg',
+  'img/la/la6.jpg',
+  'img/la/la7.jpg',
+  'img/la/la8.jpg',
+  'img/la/la8.jpg',
+  'img/la/la9.jpg'
 ];
 
-counter = 0;
+let counter = 0;
 
 const setImage = () => {
-  document.getElementById("carousel-image").src = arrayCarousel[counter];
+  document.getElementById('carousel-image').src = arrayCarousel[counter];
   counter = (counter + 1) % arrayCarousel.length;
 };
 
